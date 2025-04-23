e45631ad50c7781888d756258e76fb69507aa8de
TanPhuc77 <tanphuc77@gmail.com>
2025-04-23 15:32:56 +0700

    test

--- src/pages/materials-page/MaterialsPage.tsx
+++ src/pages/materials-page/MaterialsPage.tsx
@@ -4,7 +4,7 @@ const MaterialsPage = () => {
       <div className="flex justify-between items-center top-0 ml-6 mr-6 mb-4 mt-[22px] pt-0 pl-0">
         <h1 className="text-xl font-semibold">Materials Management</h1>
       </div>
-      <div className="flex justify-center items-center">Materials Page</div>
+      <div className="justify-center items-center">Materials Page</div>
     </>
   );
 };
